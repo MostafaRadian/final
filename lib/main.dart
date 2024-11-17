e422d44883f3288fd6207e641b485ff6a09d21f5
MostafaRadian <mostafa.batesta@gmail.com>
2024-11-17 08:55:32 +0200

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
+        //aaa
         ChangeNotifierProvider(
           create: (context) => LoginProvider(),
         ),
